ebf02c88ff946d6a3ffe6012f3c48908889e5300
Phil Davidson <digital.ninja.boy@gmail.com>
2022-11-15 18:14:46 +0000

    Project Setup
    Not sure but this is what I found :D

---
+++ Obstacle Course/Assets/Scripts/Mover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    // Cache
+
+    // Variables
+    [SerializeField] float playerSpeed = 10f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValueRaw = Input.GetAxis("Horizontal");
+        float xValue = xValueRaw * playerSpeed * Time.deltaTime;
+        float zValueRaw = Input.GetAxis("Vertical");
+        float zValue = zValueRaw * playerSpeed * Time.deltaTime;
+        transform.Translate(xValue, 0, zValue);
+    }
+}
